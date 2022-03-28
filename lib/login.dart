9df319329132926a4750b2a63e6c955abb507dd9
Kartheek Guntupalli <67854498+kartheek290700@users.noreply.github.com>
2022-03-29 00:39:56 +0530

    Add files via upload

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -6,13 +8,36 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  bool obscure = true;
+  String mail = '', username = '', password = '';
+  int page = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Row(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height*0.2,
+          ),
+          alignment: AlignmentDirectional.center,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width*0.3,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Column(
               children: [
                 TextButton.icon(
                   onPressed: () {
@@ -20,14 +45,138 @@ class _LoginState extends State<Login> {
                   },
                   icon: Icon(
                     Icons.graphic_eq_rounded,
+                    size: 30,
                   ),
                   label: Text(
                     'Social',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(page == 0 ? Colors.green : Colors.greenAccent),
+                      ),
+                      child: Text(
+                        'Login',
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          page = 0;
+                        });
+                      },
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(page != 0 ? Colors.green : Colors.greenAccent),
+                      ),
+                      child: Text(
+                        'Sign Up',
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          page = 1;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                page != 0 ? SizedBox(
+                  height: 10,
+                ) : Container(),
+                page != 0 ? TextFormField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.mail_outline,
+                      color: Colors.black,
+                    ),
+                    labelText: 'Mail',
+                    suffix: GestureDetector(
+                      onTap: () {
+                        mail != '' ? print('Send OTP') : print('Enter mail');
+                      },
+                      child: Text(
+                        'Verify',
+                      ),
+                    ),
+                  ),
+                  onChanged: (val) {
+                    setState(() {
+                      mail = val;
+                    });
+                  },
+                ) : Container(),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.person_outline,
+                      color: Colors.black,
+                    ),
+                    labelText: 'Username',
+                  ),
+                  onChanged: (val) {
+                    setState(() {
+                      username = val;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.security_outlined,
+                        color: Colors.black,
+                      ),
+                      labelText: 'Password',
+                      suffix: GestureDetector(
+                        child: Icon(
+                          obscure ? Icons.lock_open_outlined : Icons.lock_outline_rounded,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            obscure = !obscure;
+                          });
+                        },
+                      )
+                  ),
+                  obscureText: obscure ? true : false,
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                  onPressed: () {
+                    print('next');
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green),
+                  ),
+                  child: Text(
+                    'Proceed',
                   ),
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,39 @@ void main() {
     theme: ThemeData(
       primaryColor: Colors.white,
       iconTheme: IconThemeData(
-        color: Colors.black,
+        color: Colors.green,
         opacity: 1,
       ),
       textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
           foregroundColor: MaterialStateProperty.all(Colors.black),
           padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+        ),
+      ),
+      primaryIconTheme: IconThemeData(
+        color: Colors.black,
+        opacity: 1,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(5),
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(5),
+          ),
+        ),
+        isDense: true,
+        labelStyle: TextStyle(
+          color: Colors.black,
         ),
       ),
     ),
